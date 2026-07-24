7630194ae62f94b943975b31d6eddda2166f1a70
Yashwanth <yashwanth3177@gmail.com>
2026-07-24 23:50:36 +0530

    Update train.py

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 model.train(
     data="fire_smoke.yaml",
-    epochs=5,
+    epochs=30,
     imgsz=640,
     batch=16,       
     device="mps",
